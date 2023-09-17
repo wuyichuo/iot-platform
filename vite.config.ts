6d8562074a47bce28230bceddbdaaa8973cdd448
wuyichuo <3218986783@qq.com>
2023-09-17 21:54:42 +0800

    配置环境变量——后端地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
-import { join } from "path";
+import { fileURLToPath } from 'url'; // 引入Node.js的URL模块
+import path from 'path'; // 引入Node.js的path模块
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url); // 获取当前模块的文件路径
+const __dirname = path.dirname(__filename); // 获取当前模块的目录路径
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': join(__dirname, "src"),
+      '@': path.join(__dirname, 'src'), // 使用 __dirname
     }
   }
 })
