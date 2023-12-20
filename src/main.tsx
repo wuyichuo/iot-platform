071cce69ef04bf0bcb195ed7758dc8fa0cb015d3
wuyichuo <3218986783@qq.com>
2023-12-20 22:11:18 +0800

    refactor(main.tsx): 改用hashrouter 避免刷新后404

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from '@/store/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* for Redux */}
     <Provider store={store}>
       {/* for Router */}
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
